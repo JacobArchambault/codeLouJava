9658e22f882b39236ef26b2c270cad49d5088481
jacobarchambault <jacobarchambault@gmail.com>
2019-07-23 13:51:52 -0400

    Add TODOs, CSVs

---
+++ src/main/java/com/jacobarchambault/codeloujava/App.java
@@ -0,0 +1,34 @@
+package com.jacobarchambault.codeloujava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+//    TODO 1: include README:
+//    TODO 1.1: Explain what problem you are solving or question you are answering
+//    TODO 1.2: Explain how you achieved your solution, including:
+//    TODO 1.2.1: background information necessary to understand the problem
+//    TODO 1.2.2: where your data came from
+//    TODO 1.2.3: what you used from your data
+//    TODO 1.2.4: what analysis(analyses) you applied to your data
+//    TODO 1.3: Include a glossary of terms if necessary
+//    TODO 1.4: State any special requirements, dependencies, or steps to run the project
+
+//    TODO 2: Find or create any two data sets of your choosing that your application will read from. Acceptable formats include JSON, CSV, txt, SQL database.
+//    TODO 2.1: Import all or a subset of this data
+
+//    TODO 3: Application must write data TO A FILE distinct from those from which the information is imported
+
+//    TODO 4: The application must allow the user to interact with the data, e.g. via a command-line console application, e.g. 'press 1 to view ... press 2 to view ...'
+
+//    TODO 5: Your code must include comments
+
+//    TODO 6: Project must be in its own repository on Github
+
+}
