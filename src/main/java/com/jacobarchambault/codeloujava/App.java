5e00d58a06d01feebb4619dbd4f80bea448ae5d7
Jacob Archambault <jacobarchambault@gmail.com>
2021-02-09 00:17:21 -0500

    Move selection into its own Menu class

--- src/main/java/com/jacobarchambault/codeloujava/App.java
+++ src/main/java/com/jacobarchambault/codeloujava/App.java
@@ -23,7 +23,7 @@ public class App {
         FileOutputStream fos = new FileOutputStream("result.csv", false);
         PrintWriter pw = new PrintWriter(fos);
 
-        int choice = selection();
+        int choice = Menu.selection();
 
         switch (choice) {
             case 1:
@@ -71,19 +71,4 @@ public class App {
         pw.close();
     }
 
-	private static int selection() {
-		//        This code prints out the main menu to the console.
-		        System.out.println("Enter a number to choose an option:");
-		        System.out.println("1. Print a list of months where the federal funds rate was above the recorded average");
-		        System.out.println("2. Print a list of months where the federal reserve discount rate was below the recorded average");
-		        System.out.println("3. Print a list of months where the federal funds rate was greater than the federal reserve discount rate");
-		        System.out.println("4. Print a list of months where the federal reserve discount rate was greater than the federal funds rate");
-		        System.out.println("5. Exit program");
-		
-		//        Along with the code for the cases listed below, this code allows the user to choose an option from the menu.
-		        Scanner in = new Scanner(System.in);
-		        int choice = in.nextInt();
-		        in.close();
-		return choice;
-	}
 }
